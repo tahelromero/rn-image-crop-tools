f1f69228f4b377302722501326da123c201d3820
Tahel Romero <tahel.romero@gmail.com>
2025-06-28 10:46:31 -0400

    first commit

---
+++ ios/CropViewManager.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  rn-image-crop-tools
+//
+//  Created by Hunaid Hassan on 06/01/2020.
+//
+
+#import <React/RCTViewManager.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CropViewManager : RCTViewManager
+
+@end
+
+NS_ASSUME_NONNULL_END
